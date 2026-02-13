3cef28d7fc2a319a494d4e1394b46238f1db2e9c
Bansil Patel <105497726+bansil123@users.noreply.github.com>
2026-02-13 16:30:10 -0500

    Fix: Add await to test.step calls to prevent browser closing prematurely

--- src/tests/verifyHomepage.spec.ts
+++ src/tests/verifyHomepage.spec.ts
@@ -15,11 +15,11 @@ test.beforeEach(async ({ page }) => {
 test('Verify HomePage text', async({ page }) => {
     await page.waitForTimeout(5000);
 
-    test.step('Homepage has correct title', async () => {
+    await test.step('Homepage has correct title', async () => {
         await expect(page).toHaveTitle('Restful-booker-platform demo');
     });
 
-    test.step('Homepage text is correct', async () => {
+    await test.step('Homepage text is correct', async () => {
         const expectedHomePageText = "Welcome to Shady Meadows B&B\n\n" + 
                                     "Welcome to Shady Meadows, a delightful Bed & Breakfast nestled in the hills on Newingtonfordburyshire. " + 
                                     "A place so beautiful you will never want to leave. All our rooms have comfortable beds and we provide " +
@@ -28,7 +28,7 @@ test('Verify HomePage text', async({ page }) => {
         await expect(await homePage.getHomepageText()).toEqual(expectedHomePageText);
     });
 
-    test.step('Homepage menu items are correct', async () => {
+    await test.step('Homepage menu items are correct', async () => {
         const expectedMenuItems: Array<string> = ["Rooms", "Booking", "Amenities", "Location", "Contact", "Admin"]
         const actualMenuItems: Array<string> = await homePage.getHomepageMenu();
         for (let i = 0; i < expectedMenuItems.length; i++) {
@@ -40,18 +40,18 @@ test('Verify HomePage text', async({ page }) => {
 test('Verify Contact Information', async ({ page }) => {
     await page.waitForTimeout(5000);
 
-    test.step('Address is correct', async() =>{
+    await test.step('Address is correct', async() =>{
         const expectedAddress: string = "Shady Meadows B&amp;B, Shadows valley, Newingtonfordburyshire, Dilbery, N1 1AA";
         console.log(await homePage.getAddress());
         await expect (await homePage.getAddress()).toEqual(expectedAddress);
     });
 
-    test.step('Phone number is correct', async() => {
+    await test.step('Phone number is correct', async() => {
         const expectedPhoneNumber: string = "012345678901";
         await expect(await homePage.getPhone()).toEqual(expectedPhoneNumber);
     });
 
-    test.step('Email is correct', async() =>{
+    await test.step('Email is correct', async() =>{
         const expectedEmail: string = "fake@fakeemail.com";
         await expect(await homePage.getEmail()).toEqual(expectedEmail);
     });
@@ -60,14 +60,14 @@ test('Verify Contact Information', async ({ page }) => {
 test('Verify able to change Check in and Check out dates', async ({ page }) => {
     await page.waitForTimeout(5000);
 
-    test.step('Verify correct check in and check out dates are set', async() => {
+    await test.step('Verify correct check in and check out dates are set', async() => {
         console.log(new Date().getDate() + '/' + (new Date().getMonth()+1) + '/' + new Date().getFullYear());
         console.log((new Date().getDate() + 1)+ '/' + (new Date().getMonth()+1) + '/' + new Date().getFullYear());
         await expect(await homePage.getCheckInDate()).toEqual(new Date().getDate() + '/' + (new Date().getMonth()+1) + '/' + new Date().getFullYear());  
         await expect(await homePage.getCheckOutDate()).toEqual((new Date().getDate()+1) + '/' + (new Date().getMonth()+1) + '/' + new Date().getFullYear());
     });
 
-    test.step('Click Book Now button', async() =>{
+    await test.step('Click Book Now button', async() =>{
         await homePage.clickBookNow();
     });
 });
